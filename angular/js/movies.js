74088a69d3a2ab1b537459b3736ebec4beefceb2
shuchangbruce <shuchangbruce@gmail.com>
2015-10-29 17:21:39 -0700

    thursay 10/19 class

--- angular/js/movies.js
+++ angular/js/movies.js
@@ -1,6 +1,29 @@
 
-angular.module('Movies', [])
-    .controller('MoviesController',function($scope, $http) {
+angular.module('Movies', ['ui.router'])
+    .factory('moviesJSON', function($http) {
+        return $http.get('data/movies-2014.min.json');
+    })
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('list', {
+                url: '/movies',
+                templateUrl: 'views/movies-list.html',
+                controller:'MovieController'
+            })
+            .state('detail', {
+                url: '/movies/:index',
+                templateUrl: 'views/movies-detail.html',
+                controller: 'MovieDetailController'
+            });
+        $urlRouterProvider.otherwise('/movies');
+
+    })
+    .controller('MovieDetailController', function($scope, $stateParams, moviesJSON) {
+        moviesJSON.then(function(results) {
+            $scope.movie = results.data[$stateParams.index];
+        });
+    })
+    .controller('MoviesController',function($scope, moviesJSON) {
         var ratingMap = {
             'Not Rated': 0,
             'G': 1,
@@ -11,8 +34,7 @@ angular.module('Movies', [])
             'X': 6
         };
 
-        $http.get('data/movies-2014.min.json')
-        .then(function(results) {
+        moviesJSON.then(function(results) {
             $scope.movies = results.data.map(function(movie) {
                 movie.ratingOrdinal = ratingMap[movie.rating];
                 return movie;
