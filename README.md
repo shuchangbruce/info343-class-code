e5fa591855e6c410c0853cd33bea41db7f27898f
shuchangbruce <shuchangbruce@gmail.com>
2015-11-05 16:59:24 -0800

    11/5/2015

--- README.md
+++
@@ -1,2 +0,0 @@
-# info343-class-code
-Starter repos for in-class coding done during INFO 343-E, Autumn 2015
--- firebase/js/app.js
+++
@@ -1,6 +0,0 @@
-
-angular.module('ChatApp', ['firebase'])
-    .constant('firebaseUrl', 'https://info343chat.firebaseio.com/messages')
-    .controller('ChatController', function($scope) {
-        //TODO: implement our chat controller
-    });
