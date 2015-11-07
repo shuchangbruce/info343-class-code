bb3e8720b11548c37612bf29b11adbf95f816bea
shuchangbruce <shuchangbruce@gmail.com>
2015-11-07 14:30:25 -0800

    sv
    sv

---
+++ angular/js/tasks.js
@@ -0,0 +1,34 @@
+/* 
+    script for the tasks.html file 
+*/
+
+angular.module('Tasks', [])
+    .constant('taskKey', 'tasks')
+    .controller('TasksController', function($scope, taskKey) {
+        'use strict';
+        //intialize tasks property on the scope to an empty array
+        $scope.tasks = angular.fromJson(localStorage.getItem(taskKey)) || [];
+        //initialize newTask to an empty objecet
+        $scope.newTask = {};
+
+        function saveTasks() {
+            localStorage.setItem(taskKey, angular.toJson($scope.tasks));
+        }
+
+        console.log(taskKey);
+
+        //add a function to add newTask to the array
+        $scope.addTask = function() {
+            //push
+            $scope.tasks.push($scope.newTask);
+            saveTasks();
+            $scope.newTask = {};
+        };
+
+        //function to toggle task donw
+        $scope.toggleDone = function(task) {
+            task.done = !task.done;
+            saveTasks();
+        }
+    });
+
